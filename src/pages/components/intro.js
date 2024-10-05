3ee7a884f92c54b59c2fd60b9f617ce0235a0ecd
Ansh Gupta <anshgupta8004@gmail.com>
2024-10-05 21:22:34 +0530

    first commit

---
+++ src/pages/components/intro.js
@@ -0,0 +1,14 @@
+import React ,{useState} from 'react';
+import { Card, Tooltip } from 'antd';
+const Intro = () =>{
+    return(
+        <center><Card title="Uncover the Role of Greenhouse Gases in Your Neighborhood!" style={{ width: 1000 }}>
+        The development of GHG emission inventories by Parties to the Convention is supported by the scientific and methodological expertise of the Intergovernmental Panel on Climate Change (IPCC). The IPCC's guidance documents, including:
+        • The 2006 Guidelines for National Greenhouse Gas Inventories • The 1996 Revised Guidelines for National Greenhouse Gas Inventories • The 2000 Good Practice Guidance and Uncertainty Management in National Greenhouse Gas Inventories • The 2003 Good Practice Guidance on Land Use, Land-use Change and Forestry
+        <br></br>
+        
+        So you may not find information regarding a few countries, here.
+              </Card></center>
+    )
+}
+export default Intro;
